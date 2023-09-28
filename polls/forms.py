6381a554023ec0d47dbce891e0b6314c613b0ed2
KaichengChu <cbf6ah@virginia.edu>
2023-09-28 00:11:51 -0400

    Roll back the add.html, still debugging...

--- polls/forms.py
+++ polls/forms.py
@@ -6,9 +6,14 @@ class QuestionForm(forms.ModelForm):
         model = Question
         fields = ['question_text']
 
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['choice_text']
+
 ChoiceFormSet = forms.inlineformset_factory(
-    Question,  
-    Choice,    
-    fields=['choice_text'],  
+    Question,
+    Choice,
+    form=ChoiceForm,
     extra=3
 )
